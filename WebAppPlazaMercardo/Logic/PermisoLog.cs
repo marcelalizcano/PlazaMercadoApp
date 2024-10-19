72924588d4ffe24fb10df408528fa55f1e525856
deybison perez <deybison2026@gmail.com>
2024-10-19 13:05:57 -0500

    Se implementa la clase de permisoLog

--- WebAppPlazaMercardo/Logic/PermisoLog.cs
+++ WebAppPlazaMercardo/Logic/PermisoLog.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,23 @@ namespace Logic
 {
     public class PermisoLog
     {
+        PermisoDat objPer = new PermisoDat();
+
+        public DataSet showPermisos()
+        {
+            return objPer.showPermisos();
+        }
+        public bool savePermiso(string _name, string _description)
+        {
+            return objPer.savePermiso(_name, _description);
+        }
+        public bool updatePermiso(int _id, string _name, string _description)
+        {
+            return objPer.updatePermiso(_id, _name, _description);
+        }
+        public bool deletePermiso(int _idPermiso)
+        {
+            return objPer.deletePermiso(_idPermiso);
+        }
     }
 }
