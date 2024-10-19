38376b9d9ae76505aae3224afe7acaf8f7aae176
deybison perez <deybison2026@gmail.com>
2024-10-19 12:54:37 -0500

    Se implementa la clase pedidosLog

--- WebAppPlazaMercardo/Logic/PedidosLog.cs
+++ WebAppPlazaMercardo/Logic/PedidosLog.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,24 @@ namespace Logic
 {
     public class PedidosLog
     {
+        PedidosDat objPed = new PedidosDat();
+
+        public DataSet showPedidos()
+        {
+
+            return objPed.showPedidos();
+        }
+        public bool savePedidos(DateTime _date, string _state, string _specification, int _fkCliente, int _fkproducto)
+        {
+            return objPed.savePedidos(_date, _state, _specification, _fkCliente, _fkproducto);
+        }
+        public bool updatePedidos(int _id, DateTime _date, string _state, string _specification, int _fkCliente, int _fkproducto)
+        {
+            return objPed.updatePedidos(_id, _date, _state, _specification, _fkCliente, _fkproducto);
+        }
+        public bool spDeletePedidos(int _idPedidos)
+        {
+            return objPed.spDeletePedidos(_idPedidos);
+        }
     }
 }
