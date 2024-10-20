f3d92019f9c90e0a6999852fef572eddbcb8e553
marcelalizcano <linalizcanogaviria@gmail.com>
2024-10-19 22:23:55 -0500

    Se realiza DDL y tambien la clase producto

--- WebAppPlazaMercardo/Logic/ProductoLog.cs
+++ WebAppPlazaMercardo/Logic/ProductoLog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,35 @@ namespace Logic
 {
     public class ProductoLog
     {
+        ProductoDat objpro = new ProductoDat();
+
+        //Metodo para mostrar unicamente el id y la descripcion de los Productos
+        public DataSet showProductoDDL()
+        {
+
+            return objpro.showProductoDDL();
+        }
+        // metodo para mostrar los productos
+        public DataSet showProducts()
+        {
+
+            return objpro.showProducts();
+        }
+        //metodo para guardar los productos
+        public bool saveProducts(string _code, string _description, int _quantity, double _price, int _fkCategory, int _fkProvider)
+        {
+            return objpro.saveProducts(_code, _description, _quantity, _price, _fkCategory, _fkProvider);
+        }
+        //metodo para actualizar productos
+        public bool updateProducts(int _id, string _code, string _description, int _quantity, double _price, int _fkCategory, int _fkProvider)
+        {
+            return objpro.updateProducts(_id, _code, _description, _quantity, _price, _fkCategory, _fkProvider);
+        }
+        //metodo para eliminar producto
+
+        public bool deleteProducto(int _idPro)
+        {
+            return objpro.deleteProducto(_idPro);
+        }
     }
 }
